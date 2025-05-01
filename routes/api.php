2f871ec5f55a7187eae1d5de4034897641ad3e02
remdic <dev@remedic.it>
2025-05-01 15:08:35 +0200

    add main route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Remedic Api!'
+    ]);
+});
+
+
 Route::get('/test', function () {
     return response()->json([
         'message' => 'API funzionante!'
