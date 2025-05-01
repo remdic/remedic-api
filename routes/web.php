ab8e9092f5051728acd25341b0fc911a98bc5312
remdic <dev@remedic.it>
2025-05-01 16:01:47 +0200

    test autopull

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'message' => 'Remedic Api!'
+        'message' => 'Remedic Api con AUTOPULL!'
     ]);
 });
 
